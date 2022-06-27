76751c17d3c68ddebdaf24b6d455685d94dfa8a3
kn8a <88045655+kn8a@users.noreply.github.com>
2022-06-27 10:54:07 +0700

    added controllers

---
+++ controllers/companyController.js
@@ -0,0 +1,41 @@
+const company = require('../models/company')
+
+// Display list of all Authors.
+exports.company_list = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company list');
+};
+
+// Display detail page for a specific.company.
+exports.company_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company detail: ' + req.params.id);
+};
+
+// Display.company create form on GET.
+exports.company_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company create GET');
+};
+
+// Handle.company create on POST.
+exports.company_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company create POST');
+};
+
+// Display.company delete form on GET.
+exports.company_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company delete GET');
+};
+
+// Handle.company delete on POST.
+exports.company_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company delete POST');
+};
+
+// Display.company update form on GET.
+exports.company_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED:.company update GET');
+};
+
+// Handle.company update on POST.
+exports.company_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update POST');
+};
---
+++ controllers/listingController.js
@@ -0,0 +1,45 @@
+const listing = require('../models/listing')
+
+exports.index = (req,res) => {
+    res.send('NOT IMPLEMENTED: Site Home Page')
+}
+
+// Display list of all books.
+exports.listings_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Book list');
+};
+
+// Display detail page for a specific book.
+exports.listing_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing detail: ' + req.params.id);
+};
+
+// Display listing create form on GET.
+exports.listing_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing create GET');
+};
+
+// Handle listing create on POST.
+exports.listing_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing create POST');
+};
+
+// Display listing delete form on GET.
+exports.listing_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing delete GET');
+};
+
+// Handle listing delete on POST.
+exports.listing_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing delete POST');
+};
+
+// Display listing update form on GET.
+exports.listing_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: listing update GET');
+};
+
+// Handle listing update on POST.
+exports.listing_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Book update POST');
+};
